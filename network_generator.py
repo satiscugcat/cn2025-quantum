6fbe733d79a711ef4692806e5be48cf8f9385d1a
satiscugcat <aniket.mishra@iitgn.ac.in>
2025-11-09 18:01:38 +0530

    indentation error

--- network_generator.py
+++ network_generator.py
@@ -3,7 +3,7 @@ from sequence.topology.router_net_topo import RouterNetTopo
 from sequence.topology.topology import Topology as Topo
 import random
 import math
-from networkx import Graph, all_shortest_paths
+from networkx import Graph, all_shortest_paths, shortest_simple_paths
 
 def set_parameters_eta(topology: RouterNetTopo, eta: float):
     # set memory parameters
@@ -109,16 +109,16 @@ def gen_tables_shortest_path(topology: RouterNetTopo):
                     paths = all_shortest_paths(graph, src.name, dst_name)
                 else:
                     paths = list(map(lambda l: l[::-1], all_shortest_paths(graph, dst_name, src.name, weight="weight")))
-                    for p in paths:
-                        resulting_fidelity = 0.975
-                        for node in paths[1:-1]:
-                            resulting_fidelity = (resulting_fidelity-0.25)* ((4*graph[node]["efficiency"]**2 - 1)/3) * ((4*graph[node]["fidelity"] - 1)/3) + 0.25
-                        if resulting_fidelity > 0.53:
-                            next_hop = path[1]
-                            # routing protocol locates at the bottom of the stack
-                            routing_protocol = src.network_manager.protocol_stack[0]  # guarantee that [0] is the routing protocol?
-                            routing_protocol.add_forwarding_rule(dst_name, next_hop)
-                            break
+                for p in paths:
+                    resulting_fidelity = 0.975
+                    for node in paths[1:-1]:
+                        resulting_fidelity = (resulting_fidelity-0.25)* ((4*graph[node]["efficiency"]**2 - 1)/3) * ((4*graph[node]["fidelity"] - 1)/3) + 0.25
+                    if resulting_fidelity > 0.53:
+                        next_hop = path[1]
+                        # routing protocol locates at the bottom of the stack
+                        routing_protocol = src.network_manager.protocol_stack[0]  # guarantee that [0] is the routing protocol?
+                        routing_protocol.add_forwarding_rule(dst_name, next_hop)
+                        break
             except exception.NetworkXNoPath:
                 pass
 
@@ -148,15 +148,53 @@ def gen_tables_efficiency_cost(topology: RouterNetTopo):
                     paths = all_shortest_paths(graph, src.name, dst_name, weight=cost)
                 else:
                     paths = list(map(lambda l: l[::-1], all_shortest_paths(graph, dst_name, src.name)))
+                for p in paths:
+                    resulting_fidelity = 0.975
+                    for node in paths[1:-1]:
+                        resulting_fidelity = (resulting_fidelity-0.25)* ((4*graph[node]["efficiency"]**2 - 1)/3) * ((4*graph[node]["fidelity"] - 1)/3) + 0.25
+                    if resulting_fidelity > 0.53:
+                        next_hop = path[1]
+                        # routing protocol locates at the bottom of the stack
+                        routing_protocol = src.network_manager.protocol_stack[0]  # guarantee that [0] is the routing protocol?
+                        routing_protocol.add_forwarding_rule(dst_name, next_hop)
+                        break
+            except exception.NetworkXNoPath:
+                pass
+
+
+def gen_tables_kshortest_path(topology: RouterNetTopo):
+    graph = Graph()
+    for node in topology.get_nodes_by_type(RouterNetTopo.QUANTUM_ROUTER):     
+        fidelity= node.get_components_by_type("MemoryArray")[0][0].raw_fidelity
+        efficiency= node.get_components_by_type("MemoryArray")[0][0].efficiency
+        graph.add_node(node[Topo.NAME], fidelity=fidelity, efficiency=efficiency)
+    costs = {}
+    for qc in topology.qchannels:
+        router, bsm = qc.sender.name, qc.receiver
+        if bsm not in costs:
+            costs[bsm] = [router, qc.distance]
+        else:
+            costs[bsm] = [router] + costs[bsm]
+            costs[bsm][-1] += qc.distance
+    graph.add_weighted_edges_from(costs.values()) 
+    for src in topology.nodes[topology.QUANTUM_ROUTER]:
+        for dst_name in graph.nodes:
+            if src.name == dst_name:
+                continue
+            try:
+                if dst_name > src.name:
+                    paths = all_shortest_paths(graph, src.name, dst_name)
+                else:
+                    paths = list(map(lambda l: l[::-1], all_shortest_paths(graph, dst_name, src.name, weight="weight")))
                     for p in paths:
                         resulting_fidelity = 0.975
-                        for node in paths[1:-1]:
-                            resulting_fidelity = (resulting_fidelity-0.25)* ((4*graph[node]["efficiency"]**2 - 1)/3) * ((4*graph[node]["fidelity"] - 1)/3) + 0.25
-                        if resulting_fidelity > 0.53:
-                            next_hop = path[1]
-                            # routing protocol locates at the bottom of the stack
-                            routing_protocol = src.network_manager.protocol_stack[0]  # guarantee that [0] is the routing protocol?
-                            routing_protocol.add_forwarding_rule(dst_name, next_hop)
-                            break
+                    for node in paths[1:-1]:
+                        resulting_fidelity = (resulting_fidelity-0.25)* ((4*graph[node]["efficiency"]**2 - 1)/3) * ((4*graph[node]["fidelity"] - 1)/3) + 0.25
+                    if resulting_fidelity > 0.53:
+                        next_hop = path[1]
+                        # routing protocol locates at the bottom of the stack
+                        routing_protocol = src.network_manager.protocol_stack[0]  # guarantee that [0] is the routing protocol?
+                        routing_protocol.add_forwarding_rule(dst_name, next_hop)
+                        break
             except exception.NetworkXNoPath:
                 pass
